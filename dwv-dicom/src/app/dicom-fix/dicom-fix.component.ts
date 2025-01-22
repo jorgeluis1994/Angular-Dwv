e4ad9a06cc1b7571f5862793f024090ccfb450b3
jorge <luisortiz211994@gmail.com>
2025-01-22 17:20:47 -0500

    ts fix

--- dwv-dicom/src/app/dicom-fix/dicom-fix.component.ts
+++ dwv-dicom/src/app/dicom-fix/dicom-fix.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import  { App,AppOptions,DataElement,ViewConfig,ToolConfig,decoderScripts,getDwvVersion} from 'dwv';
 
@@ -8,6 +8,30 @@ import  { App,AppOptions,DataElement,ViewConfig,ToolConfig,decoderScripts,getDwv
   templateUrl: './dicom-fix.component.html',
   styleUrl: './dicom-fix.component.css'
 })
-export class DicomFixComponent {
+export class DicomFixComponent implements OnInit {
+  private dwv!: App;
+  public tools = {
+    Scroll: new ToolConfig(),
+    ZoomAndPan: new ToolConfig(),
+    WindowLevel: new ToolConfig(),
+    Draw: new ToolConfig(['Ruler']),
+};
+  
+  ngOnInit(): void {
+    // create the dwv app
+  this.dwv = new App();
+  // initialise
+  const viewConfig0 = new ViewConfig('layerGroup0');
+  const viewConfigs = {'*': [viewConfig0]};
+  const options = new AppOptions(viewConfigs);
+  options.tools = this.tools;
+  this.dwv.init(options);
+  // load dicom data
+  this.dwv.loadURLs([
+    'https://raw.githubusercontent.com/ivmartel/dwv/master/tests/data/bbmri-53323851.dcm'
+  ]);
+  }
+
+  constructor() { }
 
 }
